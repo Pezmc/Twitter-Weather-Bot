e3fcea9c5bb1b061ea0dd36d7914cb495a3f6876
Pez Cuckow <email@pezcuckow.com>
2014-05-23 23:03:03 +0100

    Add logic for different queries
    Move the logic to separate functions for clarity

--- WitWeatherParse.js
+++ WitWeatherParse.js
@@ -40,8 +40,6 @@ function processWeatherQuery(reply, callback) {
     
     console.info('Processing weather query "'+reply.msg_body+'"');
   
-    console.log(entities);
-  
     var isManchester = false;
     if(entities.location) {
         if(Array.isArray(entities.location)) {
@@ -55,43 +53,140 @@ function processWeatherQuery(reply, callback) {
         }
     }
     
-    var timePeriod = { from: new Date(), to: new Date() }
+    var timePeriod = { from: new Date(), to: new Date(), name: 'today' }
     if(entities.datetime) {
         if(Array.isArray(entities.datetime)) {
             //entities.datetime.forEach(function(value) {
             timePeriod.from = entities.datetime[0].value.from;
             timePeriod.to = entities.datetime[0].value.to;
+            timePeriod.name = entities.datetime[0].body;
             //}
         } else {
             timePeriod.from = entities.datetime.value.from;
             timePeriod.to = entities.datetime.value.to;
+            timePeriod.name = entities.datetime.body;
         }
     }
     
-   // weather_type: snow, rain, hail, thunder, sunny, cloudy
-   var weatherType = false;
-   if(entities.weather_type) {
-       weatherType = entities.weather_type;
-   }
-   
-   // weather_temperature: very cold, cold, neutral, warm, very warm
-   var weatherTemperature = false;
-   if(entities.weather_temperature) {
-       weatherTemperature = entities.weather_temperature;
-   }
-   
-   // get the weather for that date
-   var from = new Date(timePeriod.from).getTime()/1000;
-   var to = new Date(timePeriod.to).getTime()/1000;
-   var averageData = Math.round(from + to)/2;
-   
-   console.log(timePeriod);
-   console.log(new Date(timePeriod.from).getTime()/1000);
-   console.log(new Date(timePeriod.to).getTime()/1000);
-   console.log(averageData);
-   WEATHER_API.getWeatherAt(averageData, function(weather) {
-       console.log(weather);
-   });
-   
-   callback("Would have attempted to parse query");
+    // weather_type: snow, rain, hail, thunder, sunny, cloudy
+    var weatherType = false;
+    if(entities.weather_type) {
+       weatherType = entities.weather_type.value;
+    }
+    
+    // weather_temperature: very cold, cold, neutral, warm, very warm
+    var weatherTemperature = false;
+    if(entities.weather_temperature) {
+       weatherTemperature = entities.weather_temperature.value;
+    }
+    
+    // get the weather for that date
+    var from = new Date(timePeriod.from).getTime()/1000;
+    var to = new Date(timePeriod.to).getTime()/1000;
+    var averageDate = Math.round(from + to)/2;
+    
+    WEATHER_API.getWeatherAt(averageDate, function(weather) {
+      
+        var weatherTypeString = getWeatherTypeString(weatherType, weather, timePeriod.name);
+        var weatherTemperatureString = getWeatherTemperatureString(weatherTemperature, weather, timePeriod.name);      
+        
+        var weatherString = "The forecast is "+weather.description+" at "+Math.round(weather.temp)+" degrees.";
+        
+        if(weatherTypeString)
+          console.log(weatherTypeString);
+        else if(weatherTemperatureString)
+          console.log(weatherTemperatureString);
+          
+        console.log(weatherString);
+          
+    });
+    
+    callback("Would have attempted to parse query");
+}
+
+function getWeatherTypeString(type, weather, periodName) {
+
+    // weather_type: snow, rain, hail, thunder, sunny, cloudy
+    // weather API: 'clear x', 'few clouds x', 'scattered clouds x', 'broken clouds x', 'drizzle x', 'rain x', 'thunder x', 'snow x', 'mist'
+    
+    if(type) {      
+        if(type == 'snow')
+           if(weather.mapping == 'snow')
+               return ("Yes! It looks like it is going to snow " + periodName + "!");
+           else
+               return ("No, it's not going snow " + periodName + ".");
+               
+        else if(type == 'rain')
+           if(weather.mapping == 'rain')
+               return ("Unfortunately it looks like it is going to rain " + periodName + ".");
+           else if(weather.mapping == 'drizzle')
+               return ("It looks like there's going to be a little rain " + periodName + ".");
+           else
+               return ("Good news, there's no rain forecast " + periodName + "!");
+               
+        /*else if(type == 'hail' && weather.mapping == 'hail')
+           if(weather.mapping == 'hail')
+               return ("Hail is forecast "+timePeriod.name."!");
+           else
+               return ("There's no hail forecast "+timePeriod.name);*/
+               
+        else if(type == 'thunder')
+           if(weather.mapping == 'thunder')
+               return ("Yes, a thunderstorm is forecast " + periodName + "!");
+           else
+               return ("Luckily there's no thunder forecast " + periodName + ".");
+               
+        else if(type == 'sunny')
+           if(weather.mapping == 'clear')
+               return ("Sun is forecast " + periodName + ", it's going to be around "+Math.round(weather.temp)+" degrees!");
+           else if(weather.mapping == 'few clouds')
+               return ("It should be sunny " + periodName + ", but there will be a few clouds.");
+           else
+               return ("It doesn't like it's going to be sunny " + periodName + ".");
+        
+        else if(type == 'cloudy')
+           if(weather.mapping == 'scattered clouds')
+               return ("There is a little cloud forecast " + periodName + ".");
+           else if(weather.mapping == 'broken clouds')
+               return ("There will be heavy cloud " + periodName + ".");
+           else
+               return ("There's no cloud forecast " + periodName + ".");
+    }
+    
+    return "";
+}
+
+function getWeatherTemperatureString(temperature, weather, periodName) {
+    // weather_temperature: very cold, cold, neutral, warm, very warm
+    if(temperature !== false) {
+        if(weather.temp < 5)
+            if(temperature == 'very cold')
+                console.log("Yes, it's going to be very cold " + periodName + ".");
+            else
+                console.log("Luckily it's going to be "+Math.round(weather.temp)+" " + periodName + ", so not too cold!");
+                
+        else if(weather.temp < 10)
+            if(temperature == 'cold')
+                console.log("It looks like it's going to be cold " + periodName + ".");
+            else
+                console.log("It's going to be "+Math.round(weather.temp)+" degrees " + periodName + ".");
+            
+        else if(weather.temp < 15)
+            if(temperature == 'neutral')
+                console.log("It's going to be an average temperature " + periodName + ".");
+            else
+                console.log("It's going to be warmer than usual " + periodName + ".");
+            
+        else if(weather.temp < 20)
+            if(temperature == 'warm')
+                console.log("It should be warmer than usual " + periodName + ".");
+            else
+                console.log("It's going to be warmer than usual " + periodName + ".");
+            
+        else if(weather.temp >= 25)
+            if(temperature == 'very warm')
+                console.log("Yes, it should be very warm " + periodName + ".");
+            else
+                console.log("No, it's going to be very warm " + periodName + ".");
+    }
 }
