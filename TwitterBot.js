a876399c4934bc0a1d586da91e60ab199acb8097
Pez Cuckow <email@pezcuckow.com>
2014-05-24 13:46:02 +0100

    Refactor TwitterBot
    Add new methods, allow it to authenticate and use callbacks when
    waiting.

--- TwitterBot.js
+++ TwitterBot.js
@@ -1,25 +1,34 @@
 var util = require('util'),
     twitter = require('twitter');
 var twit;
+var merge = require('merge');
 
-var DUMMY_TWITTER = false;
+var DUMMY_SEARCH = false;
+var DUMMY_TWEET = false;
 var WAIT_SECONDS = 120;
 var QUERY = "";
 var TWEET_CALLBACK = null;
 var DB = null;
 
-var merge = require('merge');
 
-exports.config = function(config, debug, wait_seconds, database) {
+exports.config = function(config, wait_seconds, database) {
     twit = new twitter(config);
     WAIT_SECONDS = wait_seconds;
-    DUMMY_TWITTER = debug;
     DB = database;
 }
 
-exports.start = function(query, callback) {
+exports.dummy = function(search, tweets) {
+    DUMMY_SEARCH = search;
+    DUMMY_TWEET = tweets;
+    if(DUMMY_SEARCH)
+      console.log("Using dummy twitter search");
+    if(DUMMY_TWEET)
+      console.log("Using dummy twitter tweet");  
+}
+
+exports.start = function(query, tweet_callback, callback) {
     QUERY = query;
-    TWEET_CALLBACK = callback;
+    TWEET_CALLBACK = tweet_callback;
     
     // Ensure the database is ready for use
     DB.serialize(function() {
@@ -27,7 +36,91 @@ exports.start = function(query, callback) {
       DB.run("CREATE TABLE IF NOT EXISTS sent_tweets (id INTEGER PRIMARY KEY AUTOINCREMENT, text TEXT, related_tweet_id INTEGER)");
     });
     
-    updateTweets();
+    console.info("Authentication is needed before bot can start.");
+    requireAuthentication(function() {
+      updateTweets();
+      callback();
+    });
+
+    //exports.sleepUntilAuthComplete(function() {
+    //});
+}
+
+exports.updateActionTaken = function(tweet, actionTaken) {
+    var update = DB.prepare('UPDATE seen_tweets SET action_taken = $action WHERE id = $id');
+    update.bind({$id: tweet.id});
+    update.run();
+}
+
+function requireAuthentication(callback) {
+    var express = require('express');
+    var app = express();
+    var server;
+    
+    app.get('/', twit.gatekeeper('/login'), function(req, res){
+      res.send('Authentification successfull');
+      console.log("Auth complete, closing authentication server.");
+      server.close();
+      callback();
+    });
+    app.get('/twauth', twit.login());
+    
+    console.log('Listening on 1200 for auth requests to Twitter');
+    server = app.listen(1200);
+}
+
+/*exports.sleepUntilAuthComplete = function(callback) {
+    if(!twit.options.access_token_key || !twit.options.access_token_secret) {
+      console.warn("Authentication not complete, waiting 10 seconds before trying again");
+      setTimeout(function() {
+        console.info("Checking to see if authentication is complete");
+        exports.sleepUntilAuthComplete(callback);
+      }, 10000)
+    } else {
+      callback();
+    }
+}*/
+
+exports.sendReply = function(reply_to, message, callback) {
+
+    // Must include @ for reply, else twitter rejects it
+    if(reply_to.user.screen_name)
+      message = "@" + reply_to.user.screen_name + " " + message;
+
+    // Twitter rejects us if we provide an incorrect reply_to.id
+    params = {}
+    if(reply_to.id)
+      params.in_reply_to_status_id = reply_to.id;
+      
+    if(message.length > 140)
+        console.error("Message over 140 letters: " + message);
+
+    console.log("Attempting to send", message);
+   
+    if(DUMMY_TWEET) {
+      var dummy = { text: message };
+      logSentTweet(dummy, params);
+      callback(dummy);
+    } else {
+      twit.updateStatus(message, params, function(reply) {
+          if(reply.id) {
+            logSentTweet(reply);
+            callback(reply);
+          } else {
+            console.error("Error sending tweet", reply);
+          }
+      });
+    }
+      
+}
+
+function logSentTweet(tweet, params) {
+    if(typeof params === 'undefined')
+      params = {};
+      
+    var insert = DB.prepare("INSERT INTO sent_tweets (text, related_tweet_id) VALUES ($text, $related_id)");
+    insert.run({ $text: tweet.text, $related_id: params.in_reply_to_status_id });
+    console.log("Tweet sent successfully" + (DUMMY_TWEET ? " (Mock)" : ""));  
 }
 
 function selectTweets(params, callback) {
@@ -40,7 +133,7 @@ function selectTweets(params, callback) {
     
     params = merge(defaultParams, params);
     
-    if(DUMMY_TWITTER) callback();
+    if(DUMMY_SEARCH) callback();
     else twit.get('/search/tweets.json', params, callback);
 }
 
--- main.js
+++ main.js
@@ -1,11 +1,12 @@
 // --- Config ---
-var WAIT_SECONDS = 180;
-var DUMMY_TWITTER = false;
+var WAIT_SECONDS = 60;
+var DUMMY_TWITTER_SEARCH = false; // disable search
+var DUMMY_TWITTER_TWEET = true; // disable search
 var TWITTER_COFIG = {
     consumer_key: 'dXtdhPDWxm8xVq8Z3otvb1Dv6',
     consumer_secret: 'fshKYIXBTCQz6HkVJ7DqG00LD1ZsoN4syyNXLbZqrF8nvj1mPU',
-    access_token_key: '14605923-kYSSvN0y75setOYvv7Dq1jaAOmHFfATHMbku9uYz5',
-    access_token_secret: '9WX6w2wf6ZFLaFY9O0OJRvgAjsDjOJSnaGrlommjEjo3l'
+    //access_token_key: '14605923-kYSSvN0y75setOYvv7Dq1jaAOmHFfATHMbku9uYz5',
+    //access_token_secret: '9WX6w2wf6ZFLaFY9O0OJRvgAjsDjOJSnaGrlommjEjo3l'
 }
 var MIN_WIT_CONFIDENCE = 0.75;
 var WEATHER_CONFIG = {
@@ -14,18 +15,18 @@ var WEATHER_CONFIG = {
     mode: 'json'
 }
 var MANCHESTER_CITY_ID = 2643123;
+var TWITTER_QUERY = 'weather OR sunny OR rain OR umbrella OR snow OR hail OR warm OR cold manchester OR mcr weekend OR today OR tomorrow -rt';
 
 // --- App ---
 var sqlite3 = require('sqlite3').verbose();
 var db = new sqlite3.cached.Database('tweets.sqlite');
 
-/*var twitterbot = require('./TwitterBot.js');
-    twitterbot.config(TWITTER_COFIG, DUMMY_TWITTER, WAIT_SECONDS, db);
-    twitterbot.start('weather OR sunny OR rain OR umbrella OR snow OR hail OR warm OR cold manchester OR mcr weekend OR today OR tomorrow -rt', processNewTweet);*/
+var twitterbot = require('./TwitterBot.js');
+    twitterbot.config(TWITTER_COFIG, WAIT_SECONDS, db);
+    twitterbot.dummy(DUMMY_TWITTER_SEARCH, DUMMY_TWITTER_TWEET);
 
 var cachedweather = require('./CachedOpenWeatherAPI.js');
     cachedweather.config(WEATHER_CONFIG, MANCHESTER_CITY_ID, db);
-    cachedweather.start(15);
 
 var wit = require("./Wit.js");
 
@@ -35,19 +36,36 @@ var witparse = require("./WitWeatherParse.js");
 // --- Function ---
 function processNewTweet(tweet) {
     wit.query(tweet.text, function(reply){
-        witparse.processReply(reply, function(action) {
-            updateTweetWithActionTaken(tweet, action);
+        witparse.processReply(reply, function(message, action) {
+            if(message != false) {
+              twitterbot.sendReply(tweet, message, function(tweet) {
+                // callback on reply
+              });
+              action = "Sent tweet";
+            }
+            twitterbot.updateActionTaken(tweet, action);
         });
     });
 }
 
-function updateTweetWithActionTaken(tweet, actionTaken) {
-    var update = db.prepare('UPDATE seen_tweets SET action_taken = $action WHERE id = $id');
-    update.bind({$id: tweet.id});
-    update.run();
-}
+// --- Main ---
+cachedweather.start(30);
 
-// Main
-//updateTweets();
+twitterbot.start(TWITTER_QUERY, processNewTweet, function() {
+  testMessages = [];
+  //testMessages.push('what will the weather be Manchester next weekend?');
+  /*testMessages.push('I hope it\'s sunny in manchester this weekend!');
+  testMessages.push('will it be warm this weekend?');
+  testMessages.push('is it going to be warm tomorrow?');
+  testMessages.push('is it going to be cold this weekend?');
+  testMessages.push('is there snow forecast?');*/
+  
+  for(i=0; i < testMessages.length; i++) {
+    processNewTweet({text: testMessages[i], id: i, user: {screen_name: 'Pezmc', id: 1}});
+  }
+});
 
-processNewTweet({text: 'what will the weather be Manchester next weekend?', id: '123123123'});
+// --- Send a weather tweet every three hours
+
+
+  
