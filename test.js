233d4e9f372d56644322e5899f8f23ded55e6eb7
Pez Cuckow <email@pezcuckow.com>
2014-05-25 09:59:45 +0100

    Add some basic tests

---
+++ test.js
@@ -0,0 +1,56 @@
+
+var should = require("should");
+
+describe('Wit', function() {
+  var wit = require('./Wit.js');
+  describe('+query()', function() {
+    it('should return a valid results object', function(done){
+      wit.query('what will the weather be today?', function(results) {
+        
+        results.should.be.ok;
+        
+        results.msg_id.should.be.ok;
+        results.msg_body.should.equal('what will the weather be today?');
+        results.outcome.should.be.ok;
+        
+        done();
+      })
+    })
+  })
+  
+  describe('#witWeatherQuery', function() {
+    it('should return intent weather_query', function(done){
+      wit.query('what will the weather be today?', function(results) {
+        
+        results.msg_body.should.equal('what will the weather be today?');
+        results.outcome.intent.should.equal('weather_query');
+        
+        done();
+      })
+    })
+  })
+  
+  describe('#witWeatherStatement', function() {
+    it('should return intent weather_statement', function(done){
+      wit.query('it has been raining today', function(results) {
+        
+        results.msg_body.should.equal('it has been raining today');
+        results.outcome.intent.should.equal('weather_statement');
+        
+        done();
+      })
+    })
+  })
+  
+  describe('#witWeatherTimeQuery', function() {
+    it('should return intent weather_time_query', function(done){
+      wit.query('when will it stop raining?', function(results) {
+        
+        results.msg_body.should.equal('when will it stop raining?');
+        results.outcome.intent.should.equal('weather_time_query');
+        
+        done();
+      })
+    })
+  })
+})
