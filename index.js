a657141874d9a53923182b3168c9f7b85a54f3ee
Pez Cuckow <email@pezcuckow.com>
2014-05-26 12:15:00 +0100

    Move code to ./lib fixes #1

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // --- Config ---
-var WAIT_SECONDS = 60;
-var DUMMY_TWITTER_SEARCH = false;
+var WAIT_SECONDS = 180;
+var DUMMY_TWITTER_SEARCH = true;
 var DUMMY_TWITTER_TWEET = true;
 var DUMMY_TWITTER_MENTION_TWEET = false;
 
@@ -19,20 +19,20 @@ var WEATHER_CONFIG = {
 var MANCHESTER_CITY_ID = 2643123;
 var TWITTER_QUERY = 'weather OR sunny OR rain OR umbrella OR snow OR hail OR warm OR cold OR thunder OR @weathermcr manchester OR #mcr OR @weathermcr weekend OR today OR tomorrow OR week OR forecast OR @weathermcr -rt -from:MENnews -from:metoffice -from:ChadWeather -from:MyWeather_MAN -from:weathermcr -"Manchester, NH" -"[Manchester Weather]"';
  
-// --- App ---
+// --- Requirements ---
 var sqlite3 = require('sqlite3').verbose();
 var db = new sqlite3.cached.Database('tweets.sqlite');
 
-var twitterbot = require('./TwitterBot.js');
+var twitterbot = require('./lib/TwitterBot.js');
     twitterbot.config(TWITTER_COFIG, WAIT_SECONDS, db);
     twitterbot.dummy(DUMMY_TWITTER_SEARCH, DUMMY_TWITTER_TWEET);
 
-var cachedweather = require('./CachedOpenWeatherAPI.js');
+var cachedweather = require('./lib/CachedOpenWeatherAPI.js');
     cachedweather.config(WEATHER_CONFIG, MANCHESTER_CITY_ID, db);
 
-var wit = require("./Wit.js");
+var wit = require("./lib/Wit.js");
 
-var witparse = require("./WitWeatherParse.js");
+var witparse = require("./lib/WitWeatherParse.js");
     witparse.config(MIN_WIT_CONFIDENCE, cachedweather);
     
 // --- Function ---
