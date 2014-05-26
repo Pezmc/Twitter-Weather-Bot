4cfacf9d7752e18498509ea6c71991cdb9c0ca74
Pez Cuckow <email@pezcuckow.com>
2014-05-26 12:49:37 +0100

    Dump test messages into the test file
    Can be used later

--- test/TwitterBot.js
+++ test/TwitterBot.js
@@ -5,3 +5,17 @@ var libpath = require("./libpath.js");
 describe('TwitterBot', function() {
   var wit = require(libpath.getPath() + '/TwitterBot.js');
 })
+
+/*
+  testMessages = [];
+  //testMessages.push('what will the weather be Manchester next weekend?');
+  /*testMessages.push('I hope it\'s sunny in manchester this weekend!');
+  testMessages.push('will it be warm this weekend?');
+  testMessages.push('is it going to be warm tomorrow?');
+  testMessages.push('is it going to be cold this weekend?');
+  testMessages.push('is there snow forecast?');
+  
+  for(i=0; i < testMessages.length; i++) {
+    processNewTweet({text: testMessages[i], id: i, user: {screen_name: 'Pezmc', id: 1}});
+  }
+*/
